8daf60f190a6692c5f4ad5ff33312219a3f7d33e
gavrilov3443 <158150564+gavrilov3443@users.noreply.github.com>
2024-09-17 13:08:08 +0300

    Add files via upload

---
+++ хэви метал 2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Выберите операцию: +, -, *, /");
+        char operation = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        double result;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                Console.WriteLine($"Результат: {num1} + {num2} = {result}");
+                break;
+            case '-':
+                result = num1 - num2;
+                Console.WriteLine($"Результат: {num1} - {num2} = {result}");
+                break;
+            case '*':
+                result = num1 * num2;
+                Console.WriteLine($"Результат: {num1} * {num2} = {result}");
+                break;
+            case '/':
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    Console.WriteLine($"Результат: {num1} / {num2} = {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: Деление на ноль невозможно.");
+                }
+                break;
+            default:
+                Console.WriteLine("Ошибка: Неверная операция.");
+                break;
+        }
+    }
+}
